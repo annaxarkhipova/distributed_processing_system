e487798ffccd356b8ba044234bd34a534342b732
Anna Arkhipova <arkhipova-1997@yandex.ru>
2019-03-30 12:53:53 +0300

    done

--- client.py
+++ client.py
@@ -5,48 +5,41 @@ with open("config_client.json", "r") as read_file:
     data = json.load(read_file)
 
 
-
 class System:
     def __init__(self):
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(
-                host=data['host'], port=data['port'])) # data['port'] ???
-
+                host='localhost'))
+                #host=data['host'], port=data['port']))
         self.channel = self.connection.channel()
 
-        # объявляем  очередь результатов для полученных ответов
-        result = self.channel.queue_declare(queue='task_queue', durable=True)
+        # durable - очередь не будет потеряна при падении сервера
+        result = self.channel.queue_declare(exclusive=True)
         self.callback_queue = result.method.queue
 
         self.channel.basic_consume(self.on_response, no_ack=True,
                                    queue=self.callback_queue)
-        # basic_consume это ???
-
-
-        # Функция обратного вызова 'on_response', 
-        # исполнямая при получении каждого ответа, 
-        # выполняет довольно тривиальную задачу — 
-        # для каждого поступившего ответа она проверяет 
-        # соответствует ли correlation_id тому что мы ожидаем. 
-        # Если это так, она сохраняет ответ в self.response и 
-        # прерывает цикл
 
     def on_response(self, ch, method, props, body):
-        if self.corr_id is True:
+        if self.corr_id == props.correlation_id:
             self.response = body
 
-    def call(self, n):
+
+    def call(self):
         self.response = None
         self.channel.basic_publish(exchange='',
-                                   routing_key='rpc_queue',
+                                   routing_key='task_queue',
+                                   delivery_mode = 2, # сообщения не будут утеряны
                                    properties=pika.BasicProperties(
-                                         reply_to = self.callback_queue,
-                                         correlation_id = self.corr_id,
-                                   body='I\'m done with computing'))
+                                         reply_to=self.callback_queue,
+                                         correlation_id=self.corr_id,
+                                   body='Please, make a count for me and bring it back!'))
         while self.response is None:
             self.connection.process_data_events()
         return self.response
 
-    print('Please, make a count')
-    response = System()
-    calling = responce.call()
-    print('Got', calling)
+
+response = System()
+
+print('Sending a please to process PDF file')
+calling = response.call()
+print('Got a reply %r' % (response,))
--- computer2.py
+++ computer2.py
@@ -6,30 +6,36 @@ with open("config_computer.json", "r") as read_file:
     data = json.load(read_file)
 
 
+conn = pika.BlockingConnection(pika.ConnectionParameters(host='localhost')) #data['host'], port=data['port'])
+channel = conn.channel()
 
-conn = pika.BlockingConnection(pika.ConnectionParameters(host=data['host'], port=data['port']))
-chann = conn.channel()
-
-channel.queue_declare(queue='task_queue', durable=True)
-print('Waitin\' for new requests.')
+channel.queue_declare(queue='task_queue')
 
 
-def callback():
-    print('Recieved a mail from handler.')
+
+
+def callback_to_request(ch, method, properties, body):
+    print('Received %r' % body)
+    print('Start PDF processing')
     sleeping = data['sleep']
     time.sleep(sleeping)
-    print('Done')
-
-   
-    ch.basic_ack(delivery_tag = method.delivery_tag)
 
 
-    # no more than 1 task for a computer
-chann.basic_qos(prefetch_count=1)
-    
-# 'callback' in queue
-chann.basic_consume(сallback,
-                    queue='request_queue')
+    ch.basic_publish(exchange='',
+                     routing_key=properties.reply_to,
+                     properties=pika.BasicProperties(correlation_id= \
+                                                         properties.correlation_id),
+                     body='Finished')
+
+    # Если подписчик прекратил работу и не отправил подтверждение,
+    # RabbitMQ поймет, что сообщение не было обработано, и передаст его другому подписчику
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-chann.start_consuming()
+channel.basic_qos(prefetch_count=1) # no more than 1 queue for a consumer
+channel.basic_consume(callback_to_request, queue='task_queue')
+
+print('Awaiting RPC requests')
+channel.start_consuming()
+
+
